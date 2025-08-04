c3da9d1faf6fe0f45c5be0aa3185fac992c0468f
bugrahaneefe <bugrahaneefe@hotmail.com>
2025-08-04 22:43:03 +0300

    project creation

---
+++ spyword/spywordApp.swift
@@ -0,0 +1,17 @@
+//
+//  spywordApp.swift
+//  spyword
+//
+//  Created by Bugrahan Efe on 4.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct spywordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
